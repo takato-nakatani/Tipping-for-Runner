efcd218ff3c6bc398ef4af259856cadd4296457d
Lab's Administrator <lab-admin@h29is154.naist.jp>
2018-11-04 22:07:59 +0900

    modify app.rb

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra'   # gem 'sinatra'
+require 'json'
 require 'line/bot'  # gem 'line-bot-api'
 require 'dotenv'
+require './models/marathon.rb'
+require './models/runner.rb'
+require './models/count.rb'
+require './models/audience.rb'
+
 Dotenv.load
 
 def client
@@ -10,6 +16,28 @@ def client
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
   }
+end
+
+get '/marathon' do
+  @marathons = Marathon.all.to_json
+end
+
+post '/runner' do
+  body = request.body.read
+  if body == ''
+    status 400
+  else
+    resut = JSON.parse(body)
+    # Runner.create({
+    #   name: result[],
+    # })
+    status 201
+  end
+end
+
+
+get '/runner' do
+
 end
 
 post '/callback' do
@@ -27,11 +55,33 @@ post '/callback' do
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
-        message = {
-          type: 'text',
-          text: event.message['text']
-        }
-        client.reply_message(event['replyToken'], message)
+        text = event["message"]["text"]
+        if text == "登録"
+          message = {
+            "type": "template",
+            "altText": "this is a confirm template",
+            "template": {
+                "type": "confirm",
+                "text": "Are you audience or runner?",
+                "actions": [
+                    {
+                      "type": "message",
+                      "label": "audience",
+                      "text": "audience"
+                    },
+                    {
+                      "type": "message",
+                      "label": "runner",
+                      "text": "runner"
+                    }
+                ]
+            }
+          }
+          client.reply_message(event['replyToken'], message)
+        elsif text == "audience"
+          @user_id = event["source"]["userId"]
+          #Audience.create(audience_line_id: @user_id)
+        end
       end
     end
   }
